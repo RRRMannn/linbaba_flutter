78ff1e28b809ff69ac036c8b25e594e753c9a864
RRRMannn <975254814@qq.com>
2019-04-27 15:18:31 +0800

    阶段一

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'model/post.dart';
 
-void main() => runApp(App());
+void main() => runApp(MyApp());
 
-class App extends StatelessWidget{
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
