0673999b900e848d0ece12768bf8bd1d17287dd9
RRRMannn <975254814@qq.com>
2019-04-27 15:17:49 +0800

    阶段一

---
+++ lib/model/post.dart
@@ -0,0 +1,31 @@
+class Post{
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts=[
+  Post(
+    title: "标题1",
+    author: "作者1",
+    imageUrl: "http://a088.xyz/images/portfolio/0mmexport1554196894459.jpg"
+  ),
+  Post(
+      title: "标题2",
+      author: "作者2",
+      imageUrl: "http://a088.xyz/images/portfolio/0mmexport1554196894459.jpg"
+  ),
+  Post(
+      title: "标题3",
+      author: "作者3",
+      imageUrl: "http://a088.xyz/images/portfolio/0mmexport1554196894459.jpg"
+  )
+];
+
+
