799a5c5956b2e2bfd5ce64428549724be915a408
RRRMannn <975254814@qq.com>
2019-04-27 16:58:57 +0800

    阶段一

--- lib/main.dart
+++ lib/main.dart
@@ -20,55 +20,78 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class Home extends StatelessWidget{
+class Home extends StatefulWidget{
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return DefaultTabController(
+    return DefaultTabController(                                                //TabbarController
       length: 3,
       child: Scaffold(
-          backgroundColor: Colors.grey[100],
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text("林爸爸"),
-            elevation: 10,                    //阴影大小
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("林爸爸"),
+          elevation: 10,                    //阴影大小
 //            leading: IconButton(
 //              icon: Icon(Icons.menu),
 //              tooltip: "抽屉",
 //              onPressed: ()=>debugPrint("测试抽屉！"),
 //            ),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.search),
-                tooltip: "搜索",
-                onPressed: ()=>debugPrint("测试搜索！"),
-              ),
-            ],
-            bottom: TabBar(
-              unselectedLabelColor: Colors.black38,         //未选择颜色
-              indicatorColor: Colors.black54,               //下方提示线
-              indicatorSize: TabBarIndicatorSize.tab,
-              indicatorWeight: 2.0,
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.local_florist),),
-                Tab(icon: Icon(Icons.change_history),),
-                Tab(icon: Icon(Icons.directions_bike),),
-              ]
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: "搜索",
+              onPressed: ()=>debugPrint("测试搜索！"),
             ),
+          ],
+          bottom: TabBar(                                                       //Tabbar
+            unselectedLabelColor: Colors.black38,         //未选择颜色
+            indicatorColor: Colors.black54,               //下方提示线
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorWeight: 2.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist),),
+              Tab(icon: Icon(Icons.change_history),),
+              Tab(icon: Icon(Icons.directions_bike),),
+            ]
           ),
-          body: TabBarView(
-              children: <Widget>[
-                ListView.builder(
-                  itemCount: posts.length,
-                  itemBuilder: _listItemBuilder,
-                ),
+        ),
+        body: TabBarView(                                                       //TabbarView
+            children: <Widget>[
+              ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: _listItemBuilder,
+              ),
 //                Icon(Icons.local_florist,size: 120,color: Colors.yellow,),
-                Icon(Icons.change_history,size: 120,color: Colors.yellow,),
-                Icon(Icons.directions_bike,size: 120,color: Colors.yellow,),
-              ]
-          ),
-          drawer: MyDrawer()
+              Icon(Icons.change_history,size: 120,color: Colors.yellow,),
+              Icon(Icons.directions_bike,size: 120,color: Colors.yellow,),
+            ]
+        ),
+        drawer: MyDrawer(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.black,                   //激活状态颜色
+          currentIndex: 0,                            //当前为第几项
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              title: Text("Explore"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              title: Text("History"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text("List"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text("My"),
+            ),
+          ],
+        ),
 
       ),
     );
